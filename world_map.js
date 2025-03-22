4b14fe80860df33d862701cb8c1fbe4418bf2cbc
Radhwene <kenzizirathwene64@gmail.com>
2025-03-22 02:29:14 +0100

    Premier commit du projet MapGame

---
+++ world_map.js
@@ -0,0 +1,369 @@
+/*var a = document.getElementById('ch1').value;
+var b = document.getElementById('ch2').value;
+var c = document.getElementById('ch3').value;
+var d = document.getElementById('ch4').value;
+var e = document.getElementById('ch5').value;
+var f = document.getElementById('ch6').value;
+var g = document.getElementById('ch7').value;
+var vid="..."
+
+function change(sel){
+    var vid = "..."; // Valeur à utiliser pour vider le champ
+    
+    if (sel === 'ch1') {
+        a.value = vid;
+    } else if (sel === 'ch2') {
+        b.value = vid;
+    } else if (sel === 'ch3') {
+        c.value = vid;
+    } else if (sel === 'ch4') {
+        d.value = vid;
+    } else if (sel === 'ch5') {
+        e.value = vid;
+    } else if (sel === 'ch6') {
+        f.value = vid;
+    } else if (sel === 'ch7') {
+        g.value = vid;
+    }
+}
+
+
+
+function dd(){
+alert("fv");
+
+
+}
+
+
+
+var a = document.getElementById('ch1').value;
+var b = document.getElementById('ch2').value;
+var c = document.getElementById('ch3').value;
+var d = document.getElementById('ch4').value;
+var e = document.getElementById('ch5').value;
+var f = document.getElementById('ch6').value;
+var g = document.getElementById('ch7').value;
+var vid="..."
+
+/*function change(sel){
+    var vid = "..."; // Valeur à utiliser pour vider le champ
+    
+    if (sel === 'ch1') {
+        a.value = vid;
+    } else if (sel === 'ch2') {
+        b.value = vid;
+    } else if (sel === 'ch3') {
+        c.value = vid;
+    } else if (sel === 'ch4') {
+        d.value = vid;
+    } else if (sel === 'ch5') {
+        e.value = vid;
+    } else if (sel === 'ch6') {
+        f.value = vid;
+    } else if (sel === 'ch7') {
+        g.value = vid;
+    }
+}
+
+
+
+function dd(){
+alert("vous aller voire la correction");
+var body = document.body;
+
+            // Changer l'image de fond
+            body.style.backgroundImage = "url('world_map_name.png.jpg')";
+
+}
+
+function change(sel){
+    
+var a=document.map.ch1.value;
+var b=document.map.ch2.value;
+var c=document.map.ch3.value;
+var d=document.map.ch4.value;
+var e=document.map.ch5.value;
+var f=document.map.ch6.value;
+var g=document.map.ch7.value;
+
+var 
+je veut changer l option du ancienne quand l'utilisateur choisir la mémé dans une autre select    
+if (sel==a) {
+   azerty.selectedIndex = 1;*/
+
+
+
+   // Sélectionnez tous les éléments avec la classe "chouse"
+var selectElements = document.querySelectorAll('.chouse');
+
+// Parcourez chaque élément et ajoutez un écouteur d'événements change
+selectElements.forEach(function(selectElement) {
+  selectElement.addEventListener('change', function() {
+    var audio = document.getElementById('sound');
+    audio.currentTime = 0; // Réinitialiser la lecture à partir du début pour permettre une lecture répétée
+    audio.play();
+  });
+});
+
+
+
+function change1(sel) {
+   
+    var b=document.map.ch2.value;
+    var c=document.map.ch3.value;
+    var d=document.map.ch4.value;
+    var e=document.map.ch5.value;
+    var f=document.map.ch6.value;
+    var g=document.map.ch7.value;
+    
+
+if (sel==b) { document.getElementById("ch2").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch3").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch4").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch5").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch6").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch7").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+
+
+
+
+
+
+function change2(sel) {
+   
+    var a=document.map.ch1.value;
+    var c=document.map.ch3.value;
+    var d=document.map.ch4.value;
+    var e=document.map.ch5.value;
+    var f=document.map.ch6.value;
+    var g=document.map.ch7.value;
+if (sel==a) { document.getElementById("ch1").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch3").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch4").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch5").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch6").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch7").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+
+
+function change3(sel) {
+   
+    var b=document.map.ch1.value;
+    var c=document.map.ch2.value;
+    var d=document.map.ch4.value;
+    var e=document.map.ch5.value;
+    var f=document.map.ch6.value;
+    var g=document.map.ch7.value;
+if (sel==b) { document.getElementById("ch1").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch2").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch4").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch5").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch6").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch7").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+
+function change4(sel) {
+   
+    var b=document.map.ch1.value;
+    var c=document.map.ch2.value;
+    var d=document.map.ch3.value;
+    var e=document.map.ch5.value;
+    var f=document.map.ch6.value;
+    var g=document.map.ch7.value;
+if (sel==b) { document.getElementById("ch1").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch2").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch3").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch5").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch6").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch7").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+
+function change5(sel) {
+   
+    var b=document.map.ch1.value;
+    var c=document.map.ch2.value;
+    var d=document.map.ch3.value;
+    var e=document.map.ch4.value;
+    var f=document.map.ch6.value;
+    var g=document.map.ch7.value;
+if (sel==b) { document.getElementById("ch1").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch2").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch3").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch4").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch6").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch7").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+
+
+function change6(sel) {
+   
+    var b=document.map.ch1.value;
+    var c=document.map.ch2.value;
+    var d=document.map.ch3.value;
+    var e=document.map.ch4.value;
+    var f=document.map.ch5.value;
+    var g=document.map.ch7.value;
+if (sel==b) { document.getElementById("ch1").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch2").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch3").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch4").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch5").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch7").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+
+
+function change7(sel) {
+   
+    var b=document.map.ch1.value;
+    var c=document.map.ch2.value;
+    var d=document.map.ch3.value;
+    var e=document.map.ch4.value;
+    var f=document.map.ch5.value;
+    var g=document.map.ch6.value;
+if (sel==b) { document.getElementById("ch1").selectedIndex = 0;  }
+else {if (sel==c) {document.getElementById("ch2").selectedIndex = 0;}
+else {if (sel==d) {document.getElementById("ch3").selectedIndex = 0;}
+else {if (sel==e) {document.getElementById("ch4").selectedIndex = 0;}
+else {if (sel==f) {document.getElementById("ch5").selectedIndex = 0;}
+else {if (sel==g) {document.getElementById("ch6").selectedIndex = 0;}  }}}}}}
+
+
+
+
+
+function correction(){
+    alert("vous allez voire la correction");
+    var body = document.body;
+    
+                // Changer l'image de fond
+                body.style.backgroundImage = "url('world_map_name.png.jpg')";
+    
+
+             
+                $("#wel, #bet, #form, #title").hide();     
+    }
+
+
+
+
+    function teste() {
+
+        var a=document.map.ch1.value;
+        var b=document.map.ch2.value;
+        var c=document.map.ch3.value;
+        var d=document.map.ch4.value;
+        var e=document.map.ch5.value;
+        var f=document.map.ch6.value;
+        var g=document.map.ch7.value;
+        var n=0;
+
+if (a=="North Ameria") {n++;
+    document.getElementById('ch1').style.border= "6px outset lime";
+    document.getElementById('ch1').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";   }
+     else {
+    document.getElementById('ch1').style.border= "6px outset red";
+    document.getElementById('ch1').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+
+    
+
+    if (b=="South America") {n++;
+        document.getElementById('ch2').style.border= "6px outset lime";
+        document.getElementById('ch2').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";}
+         else {
+        document.getElementById('ch2').style.border= "6px outset red";
+        document.getElementById('ch2').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+    
+
+    
+        if (c=="Africa") {n++;
+            document.getElementById('ch3').style.border= "6px outset lime";
+            document.getElementById('ch3').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";}
+             else {
+            document.getElementById('ch3').style.border= "6px outset red";
+            document.getElementById('ch3').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+        
+
+            if (d=="Europe") {n++;
+                document.getElementById('ch4').style.border= "6px outset lime";
+                document.getElementById('ch4').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";}
+                 else {
+                document.getElementById('ch4').style.border= "6px outset red";
+                document.getElementById('ch4').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+            
+
+
+
+                if (e=="Asia") {n++;
+                    document.getElementById('ch5').style.border= "6px outset lime";
+                    document.getElementById('ch5').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";}
+                     else {
+                    document.getElementById('ch5').style.border= "6px outset red";
+                    document.getElementById('ch5').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+                
+
+                    if (f=="Australia") {n++;
+                        document.getElementById('ch6').style.border= "6px outset lime";
+                        document.getElementById('ch6').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";}
+                         else {
+                        document.getElementById('ch6').style.border= "6px outset red";
+                        document.getElementById('ch6').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+                    
+
+                        if (g=="Intrctica") {n++;
+                            document.getElementById('ch7').style.border= "6px outset lime";
+                            document.getElementById('ch7').style.boxShadow = "0px 8px 16px rgb(33, 252, 91)";}
+                             else {
+                            document.getElementById('ch7').style.border= "6px outset red";
+                            document.getElementById('ch7').style.boxShadow = "0px 8px 16px rgba(255, 8, 8, 0.881)";}
+                        
+
+        
+
+
+                            if (n == 7) {
+                                alert("Excellent :) Votre résultat est " + n + "/7");
+                            } else if (n < 7 && n >= 5) {
+                                alert("Très proche. Votre résultat est " + n + "/7");
+                            } else if (n < 5 && n >= 3) {
+                                alert("Pas mal. Votre résultat est " + n + "/7");
+                            } else if (n < 3 && n > 1) {
+                                alert("Vous êtes nul en géographie. Votre résultat est " + n + "/7");
+                            } else {
+                                alert("reellment ?. Votre résultat est " + n + "/7");
+                            }
+                            
+
+
+
+
+
+
+
+    }
+    
